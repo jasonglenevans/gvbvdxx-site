8a84ded1a639df0bb390809d95884cb62b2114aa
jasonglenevans <56980043+jasonglenevans@users.noreply.github.com>
2021-09-14 20:42:43 -0400

    Create update_check.js

---
+++ downloader-files/update_check.js
@@ -0,0 +1,16 @@
+//this is a update check for the app named "gvbvdxx game downloader" version check files can be modifed by simply changing the site data
+
+confirmed = window.confirm ("do you want to check for updates (the app has loaded the update check file meaning that your online)")
+if (confirmed)
+ {
+  window.alert ("version check will now be extucuted")
+   if (appversion < 1)
+   {
+   confirmed2 = window.confirm ("you seem to have an outdated version of this app do you want to install it?")
+    if (appconfirmed2)
+    {
+     window.alert ("the app will redirect you to the download page for this site")
+     window.location.replace ("home.html")
+    }
+   }
+ }
